c15ae792fefb5dd8ecc1696148662e69296aa20d
WilsonTeofilo <wilsonteofilosouza@live.com>
2025-07-18 01:05:28 -0300

    versao1

---
+++ scripiti.js
@@ -0,0 +1,9 @@
+let numero = window.document.getElementById("num")
+let resultado = window.document.getElementById("seletor")
+
+function tabuada(){
+    if (numero.value.length == 0){
+        window.alert(`digite um número, caixa vazia.`)
+    }
+
+}
