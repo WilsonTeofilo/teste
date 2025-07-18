6320583d5d6cb0501d95b67a8fa9481c2ff795f0
WilsonTeofilo <wilsonteofilosouza@live.com>
2025-07-18 01:15:21 -0300

    script feito, falta só css

--- scripiti.js
+++ scripiti.js
@@ -4,6 +4,15 @@ let resultado = window.document.getElementById("seletor")
 function tabuada(){
     if (numero.value.length == 0){
         window.alert(`digite um número, caixa vazia.`)
+    }else{
+resultado.innerHTML= ""
+    let numeror = Number(numero.value)
+    for (contador = 0; contador <=10; contador++){
+        var elemento = window.document.createElement("option")
+        elemento.text= (`${numeror} x ${contador} = ${numeror*contador}`)
+        elemento.value=`tab ${contador}`
+        resultado.appendChild(elemento)
     }
+    } 
 
 }
